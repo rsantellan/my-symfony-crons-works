b8d9984baa8173ff2389f97367a7a97a48186e19
rsantellan@gmail.com <rsantellan@gmail.com@4a2db006-cd1e-d9d2-6dfe-fb94cc9fdb6e>
2014-01-22 18:07:02 +0000

    Se agregan nuevos modelos.

---
+++ lib/form/doctrine/base/BasecuentaForm.class.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * cuenta form base class.
+ *
+ * @method cuenta getObject() Returns the current form's model object
+ *
+ * @package    jardin
+ * @subpackage form
+ * @author     Gaston Caldeiro
+ * @version    SVN: $Id: sfDoctrineFormGeneratedTemplate.php 29553 2010-05-20 14:33:00Z Kris.Wallsmith $
+ */
+abstract class BasecuentaForm extends BaseFormDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'id'         => new sfWidgetFormInputHidden(),
+      'nombre'     => new sfWidgetFormInputText(),
+      'created_at' => new sfWidgetFormDateTime(),
+      'updated_at' => new sfWidgetFormDateTime(),
+    ));
+
+    $this->setValidators(array(
+      'id'         => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
+      'nombre'     => new sfValidatorString(array('max_length' => 64)),
+      'created_at' => new sfValidatorDateTime(),
+      'updated_at' => new sfValidatorDateTime(),
+    ));
+
+    $this->widgetSchema->setNameFormat('cuenta[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'cuenta';
+  }
+
+}
