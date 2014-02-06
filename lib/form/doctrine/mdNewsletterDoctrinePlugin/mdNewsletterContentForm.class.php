8f928155bb69382d8a760572b161c3f8780ca622
rsantellan@gmail.com <rsantellan@gmail.com@4a2db006-cd1e-d9d2-6dfe-fb94cc9fdb6e>
2014-02-06 18:43:37 +0000

    Se corrige que aparezcan los alumnos activos en el newsletter el tiny y los precios.

--- lib/form/doctrine/mdNewsletterDoctrinePlugin/mdNewsletterContentForm.class.php
+++ lib/form/doctrine/mdNewsletterDoctrinePlugin/mdNewsletterContentForm.class.php
@@ -15,8 +15,16 @@ class mdNewsletterContentForm extends PluginmdNewsletterContentForm
     parent::configure();
 
     $this->widgetSchema['subject'] = new sfWidgetFormInputText();
-    
-    $this->widgetSchema['body'] = new sfWidgetFormTextareaTinyMCE(array('height' => '400'), array('rows' => 15));
+    $dataConfig = '
+      plugins : "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,wordcount,advlist, spellchecker",
+      theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,formatselect,fontselect,fontsizeselect,|",
+      theme_advanced_buttons2 : "bullist,numlist,|,link,unlink,code,|,insertdate,inserttime,preview,|,forecolor,backcolor,|fullscreen,|,undo,redo,cut,copy,paste,pastetext,pasteword,|",
+      theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,image, cleanup",
+      theme_advanced_toolbar_location : "top",
+      theme_advanced_toolbar_align : "left",
+      theme_advanced_statusbar_location : "bottom",
+      theme_advanced_resizing : true,';
+    $this->widgetSchema['body'] = new sfWidgetFormTextareaTinyMCE(array('height' => '400', 'config' => $dataConfig), array('rows' => 15));
     $this->validatorSchema['body'] = new sfValidatorString(array('required' => false));
   }
   
