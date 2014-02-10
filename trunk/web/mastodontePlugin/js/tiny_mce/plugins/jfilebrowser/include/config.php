ea01d15e9f035bd86d89e878a00a6c1fd0b1e3ce
rsantellan@gmail.com <rsantellan@gmail.com@4a2db006-cd1e-d9d2-6dfe-fb94cc9fdb6e>
2014-02-10 13:10:09 +0000

    Se borran los hard links de los plugins.

--- trunk/web/mastodontePlugin/js/tiny_mce/plugins/jfilebrowser/include/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-define('JFILEBROWSER_PATH', $_SERVER['DOCUMENT_ROOT'] . '/uploads/jfilebrowser');
-
-define('RELATIVE_PATH_TO_LAYOUT', $_SERVER['DOCUMENT_ROOT'].'/../plugins/mdTinyFileBrowserPlugin/modules/jfilebrowser/templates/layout.php');
