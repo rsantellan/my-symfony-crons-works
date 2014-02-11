968ba4b9d9df192af6b2606385df0ba908a6eb20
rsantellan@gmail.com <rsantellan@gmail.com@4a2db006-cd1e-d9d2-6dfe-fb94cc9fdb6e>
2014-02-11 10:15:07 +0000

    Se agrega nuevo sistema de factura.
    Correr php symfony doctrine:clean cuando se termine.

---
+++ lib/filter/doctrine/facturaUsuarioFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * facturaUsuario filter form.
+ *
+ * @package    jardin
+ * @subpackage filter
+ * @author     Gaston Caldeiro
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class facturaUsuarioFormFilter extends BasefacturaUsuarioFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/form/doctrine/facturaFinalForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * facturaFinal form.
+ *
+ * @package    jardin
+ * @subpackage form
+ * @author     Gaston Caldeiro
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class facturaFinalForm extends BasefacturaFinalForm
+{
+  public function configure()
+  {
+  }
+}
